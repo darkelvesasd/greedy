4911351863d00a8d7afad30a90ef311fcdda61e2
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-09-19 20:05:25 +0800

    Update 870. 优势洗牌.cpp

--- 870. 优势洗牌.cpp
+++ 870. 优势洗牌.cpp
@@ -4,7 +4,6 @@ vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
 	int n = nums1.size();
 	vector<int>ret(n);
 	vector<int>v1(n);
-	vector<int>r(n);
 	int left = 0;
 	int right = n - 1;
 	for (int i = 0; i < n; i++)
@@ -16,22 +15,17 @@ vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
 		return nums2[x] < nums2[y];
 	});
 	sort(nums1.begin(), nums1.end());
-	sort(nums2.begin(), nums2.end());
 	for (int i=0; i<n; i++)
 	{
-		if (nums1[i] <= nums2[left])
+		if (nums1[i] <= nums2[v1[left]])
 		{
-			ret[right--] = nums1[i];
+			ret[v1[right--]] = nums1[i];
 		}
 		else
 		{
-			ret[left++] = nums1[i];
+			ret[v1[left++]] = nums1[i];
 		}
 	}
-	for (int i = 0; i < n; i++)
-	{
-		r[v1[i]] = ret[i];
-	}
-	return r;
+	return ret;
 }
 };
