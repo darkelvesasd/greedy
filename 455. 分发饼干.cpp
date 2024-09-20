3b72af22b92a59a4664ada0c0f1b3858642bf767
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-09-20 14:01:40 +0800

    Update 455. 分发饼干.cpp

--- 455. 分发饼干.cpp
+++ 455. 分发饼干.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
 int findContentChildren(vector<int>& g, vector<int>& s) {
-	int n = s.size();
 	int m = g.size();
-	if (n == 0)
-	{
-		return 0;
-	}
 	sort(g.begin(), g.end());
 	sort(s.begin(), s.end());
 	int left = 0;
