c0d1e3c41e9fad253e4f0020f65cdfb5260554af
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-10-06 21:57:59 +0800

    Create 991. 坏了的计算器.cpp

---
+++ 991. 坏了的计算器.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int brokenCalc(int startValue, int target) {
+	int count = 0;
+	while (target > startValue)
+	{
+		if (target % 2 == 1)
+		{
+			target++;
+		}
+		else
+		{
+			target /= 2;
+		}
+		count++;
+	}
+	return startValue-target+count;
+}
+};
